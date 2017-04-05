2921da7dfd16f060742c36b73447e6d4bd1cfac6
Moreno Gentili <brightsoul@tiscali.it>
2017-04-05 08:00:24 +0200

    Aggiunta la specifica del blocco note

--- Tests/OrologioTest.cs
+++ Tests/OrologioTest.cs
@@ -18,6 +18,13 @@ namespace Scacchi.Tests {
         Assert.Equal(TimeSpan.FromMinutes(5), orologio1.TempoResiduoGiocatore2);
         }
 
+        [Theory]
+        [InlineData(5, 10)]
+        [InlineData(3, 67)]
+        public void Doppio(int valore1, int valoreatteso) {
+            Assert.Equal(valoreatteso, valore1*2);
+        }
+
     }
 
 }
