6238365deabe75fdf0a73fe2ae0d4aba145f1a30
Michele Biondi <michele.biondi@studenti.unicam.it>
2017-04-06 20:35:34 +0200

    Fase green test dell'alfiere

--- Modello/Pezzi/Alfiere.cs
+++ Modello/Pezzi/Alfiere.cs
@@ -8,7 +8,7 @@ namespace Scacchi.Modello.Pezzi{
         public Alfiere(Colore colore){
             this.colore = colore;
         }
-        
+
         public Colore Colore{
             get{
                 return colore;
@@ -17,7 +17,27 @@ namespace Scacchi.Modello.Pezzi{
 
         public bool PuòMuovere(Colonna colonnaPartenza, Traversa traversaPartenza, Colonna colonnaArrivo, Traversa traversaArrivo)
         {
-            throw new NotImplementedException();
+            int spostamentoTraversa = ValoreAssoluto(traversaArrivo - traversaPartenza);
+            int spostamentoColonna = ValoreAssoluto(colonnaArrivo - colonnaPartenza);
+
+            
+            if(spostamentoColonna == 0 && spostamentoTraversa == 0){ // Non può restare fermo
+                return false;
+            } else {
+                if( (spostamentoColonna - spostamentoTraversa) == 0){
+                    return true;
+                } else {
+                    return false;
+                }
+            }   
+
+        }
+
+        private int ValoreAssoluto(int numero){
+            if(numero < 0)
+                return -numero;
+            
+            return numero;
         }
     }
 }
