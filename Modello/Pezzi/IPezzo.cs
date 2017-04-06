cf3a91967698c478f544ad6021fea25b250fa5d5
Moreno Gentili <brightsoul@tiscali.it>
2017-04-06 08:00:28 +0200

    aggiornato IPezzo

--- Modello/Pezzi/IPezzo.cs
+++ Modello/Pezzi/IPezzo.cs
@@ -4,7 +4,7 @@ namespace Scacchi.Modello.Pezzi
     {
         //Muovi restituisce un bool che rappresenta l'esito della mossa
         //Se true: il pezzo ha potuto muoversi nella casa di destinazione
-        //Se false: c'era qualcosa che gli impediva il movimento, come un pezzo "amico" dello stesso colore
+        //Se false: c'era qualcosa che gli impediva il movimento, come un pezzo "amico" dello stesso colore o un altro pezzo posto in mezzo tra partenza e destinazione
         //oppure la destinazione non era raggiungibile dal pezzo attuale (es. è stato chiesto ad una torre di muoversi in diagonale)
         bool PuòMuovere(Colonna colonnaPartenza,
             Traversa traversaPartenza,
