6f8ac172198b3d19cfdb889d7fc64c18656fd5c1
Stach96 <lorenzo.stacchio@studenti.unicam.it>
2017-04-06 12:39:40 +0200

    Modifico la classe torre in modo tale che il test del movimento verticale passi!

--- Modello/Pezzi/Torre.cs
+++ Modello/Pezzi/Torre.cs
@@ -8,20 +8,35 @@ namespace Scacchi.Modello.Pezzi
         {
             this.colore = colore;
         }
-        public Colore Colore {
-            get{
+        public Colore Colore
+        {
+            get
+            {
                 return this.colore;
             }
         }
         public bool PuòMuovere(Colonna colonnaPartenza, Traversa traversaPartenza, Colonna colonnaArrivo, Traversa traversaArrivo)
         {
-            
+
             //se la traversa di partenza è la stessa traversa di arrivo allora la torre e le colonne sono diverse
             //si sta muovendo orizzontalmente
-            if(((int)traversaPartenza == (int)traversaArrivo) && (colonnaPartenza!=colonnaArrivo)){
+            if (((int)traversaPartenza == (int)traversaArrivo) && (colonnaPartenza != colonnaArrivo))
+            {
                 return true;
             }
-            return false;
+            //Se la traversa di partenza è diversa da quella di fine e la colonna è la stessa
+            // la torre si sta muovendo verticalmente
+            else if (((int)traversaPartenza != (int)traversaArrivo) && (colonnaPartenza == colonnaArrivo))
+            {
+                return true;
+
+            }
+            //in tutti gli altri casi ritorno false
+            else
+            {
+                return false;
+
+            }
         }
     }
 }
