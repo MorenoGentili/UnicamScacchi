739d212330accd2a216f0e2de63f94c2636d96e1
Stach96 <lorenzo.stacchio@studenti.unicam.it>
2017-04-06 17:42:15 +0200

    Implemento il movimento obliquo, verticale e orizzontale del re. Ora tutti i test passano.

--- Modello/Pezzi/Re.cs
+++ Modello/Pezzi/Re.cs
@@ -21,6 +21,21 @@ namespace Scacchi.Modello.Pezzi
 
         public bool PuòMuovere(Colonna colonnaPartenza, Traversa traversaPartenza, Colonna colonnaArrivo, Traversa traversaArrivo)
         {
+            int differenzaTraverse = Math.Abs((int)traversaArrivo - (int)traversaPartenza);
+            int differenzaColonne = Math.Abs((int)colonnaArrivo - (int)colonnaPartenza);
+
+            //implemento il movimento verticale
+            if (differenzaColonne==0 && differenzaTraverse==1) {
+                return true;
+            }
+            //il movimento orizzontale
+            else if(differenzaTraverse==0 && differenzaColonne==1){
+                return true;
+            }
+            //movimento diagonale
+            else if(differenzaColonne==1 && differenzaTraverse==1){
+                return true;
+            }
             return false;
         }
     }
