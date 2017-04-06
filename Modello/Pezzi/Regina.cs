a2d42e61fe8c0a0a2e4c1a7e308c0354228947a4
Stach96 <lorenzo.stacchio@studenti.unicam.it>
2017-04-06 16:31:51 +0200

    Implemento il movimento diagonale e il test passa!

--- Modello/Pezzi/Regina.cs
+++ Modello/Pezzi/Regina.cs
@@ -20,16 +20,43 @@ namespace Scacchi.Modello.Pezzi
         public bool PuòMuovere(Colonna colonnaPartenza, Traversa traversaPartenza, Colonna colonnaArrivo, Traversa traversaArrivo)
         {
             //faccio in modo che la regina possa muoversi verticalmente dove vuole
-            if (colonnaArrivo == colonnaPartenza && traversaPartenza!=traversaArrivo)
+            if (colonnaArrivo == colonnaPartenza && traversaPartenza != traversaArrivo)
             {
                 return true;
             }
             //faccio in modo che la regina possa muoversi lateralmente
-            if (colonnaArrivo != colonnaPartenza && (traversaPartenza==traversaArrivo))
+            else if (colonnaArrivo != colonnaPartenza && (traversaPartenza == traversaArrivo))
             {
                 return true;
             }
-
+            else if (colonnaArrivo != colonnaPartenza && traversaPartenza != traversaArrivo)
+            {
+                //sto tentando di muovermi lateralmente
+                //ho escluso gran parte dell'area ammissibile in cui l'alfiere può andare ma non tutta!
+                //mi salvo su due variabili la traversa di partenza e la colonna di partenza
+                int IndicecolonnaPartenza = (int)colonnaPartenza;
+                //definisco come rage destro e sinistro il numero di colonne e traverse su cui
+                //l'alfiere può andare
+                int colonnaRangeDestro = 8 - IndicecolonnaPartenza;
+                int colonnaRangeSinistro = IndicecolonnaPartenza - 1;
+                //controllo se il range di destinazione è accettabile
+                //concetto di +1 +1
+                for (int i = 1; i <= colonnaRangeDestro; i++)
+                {
+                    if (Math.Abs(colonnaArrivo - colonnaPartenza) == i && Math.Abs(traversaArrivo - traversaPartenza) == i)
+                    {
+                        return true;
+                    }
+                }
+                for (int i = 1; i <= colonnaRangeSinistro; i++)
+                {
+                    if (Math.Abs(colonnaArrivo - colonnaPartenza) == i && Math.Abs(traversaArrivo - traversaPartenza) == i)
+                    {
+                        return true;
+                    }
+                }
+            }
+            //Se sono qui la regina si sta spostando in una posizione non ammissibile!
             return false;
         }
     }
