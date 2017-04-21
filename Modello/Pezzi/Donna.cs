76a0b55d9f898a7fa96bb4d759fece0acbbf73f1
Lorenzo Stacchio <Lorenzo Stacchio>
2017-04-21 16:32:23 +0200

    Implementato movimento obliquo della donna.

--- Modello/Pezzi/Donna.cs
+++ Modello/Pezzi/Donna.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Scacchi.Modello.Pezzi
 {
@@ -25,21 +26,59 @@ namespace Scacchi.Modello.Pezzi
             Traversa traversaArrivo,
             IEnumerable<ICasa> listaCase = null)
         {
-            var differenzaColonne = colonnaPartenza - colonnaArrivo;
+
+            listaCase = listaCase ?? Enumerable.Empty<ICasa>();
+            //prendo la casa di partenza
+            ICasa casaPartenza = listaCase.SingleOrDefault(casa => casa.Colonna == colonnaPartenza
+             && casa.Traversa == traversaPartenza
+             && casa.PezzoPresente == this);
+            //prendo la casa di arrivo senza specificare se ci sia un pezzo o meno
+            ICasa casaArrivo = listaCase.SingleOrDefault(casa => casa.Colonna == colonnaArrivo
+                         && casa.Traversa == traversaArrivo);
+            //controllo che non ci siano pezzi tra me e l'arrivo              
+            //prendo le differenze tra colonne e traverse            
+            var differenzaColonne = (int)colonnaPartenza - (int)colonnaArrivo;
             var differenzaTraverse = (int)traversaPartenza - (int)traversaArrivo;
+            //confronto le colonne e le traverse
             var stessaColonna = colonnaPartenza == colonnaArrivo;
             var stessaTraversa = traversaPartenza == traversaArrivo;
+            //controllo se sono rimasto fermo
             if (differenzaColonne == 0 && differenzaTraverse == 0)
                 return false;
-            if (((stessaTraversa && !stessaColonna)
-            || (stessaColonna && !stessaTraversa)) 
-            || ((Math.Abs(differenzaColonne) - Math.Abs(differenzaTraverse)) == 0))
+            //prima di tutto devo capire se mi sto muovendo verticalmente, orizzontalmente o in obliquo
+            if ((Math.Abs(differenzaColonne) - Math.Abs(differenzaTraverse)) == 0)
             {
-                return true;
-            }
-            else
-            {
+                //mi sto muovendo in obliquo
+                //controllo che davanti a me non ci siano pezzi di alcun genere
+                int pezziTrovati = (listaCase.Where(casa => casa.Colonna != casaPartenza.Colonna
+                     && casa.Traversa != casaPartenza.Traversa && casa.PezzoPresente != null)).Count();
+                if (pezziTrovati > 1)
+                {
+                    //ci sono più di un pezzo tra me e la destinazione, non è possibile andare oltre!
+                    return false;
+                }
+                else if(pezziTrovati==1){
+                    //devo controllare la posizione di questo pezzo
+                    if(casaArrivo.PezzoPresente==null){
+                        //allora il pezzo sta in mezzo tra me e la destinazione 
+                        return false;
+                    }
+                    else{
+                        return true;
+                    }
+                }
+                else if (pezziTrovati==0){
+                    //non c'è nessun pezzo tra me e la destinazione posso andare lì
+                    return true;
+                }
                 return false;
+
+            }
+            else if (((stessaTraversa && !stessaColonna){
+
+            }
+            else if(stessaColonna && !stessaTraversa)){
+
             }
         }
     }
